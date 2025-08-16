6e2356286ce22ee42a38a97cf8672f64535ef0c3
polexa84 <polexa84@gmail.com>
2025-08-17 00:24:24 +0300

    Добавлена модели созданы миграции

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     # Дополнительные поля для модели пользователя, если нужно
+    pass
 
     def __str__(self):
         return self.username
